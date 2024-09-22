7f10345966100549415400fd0c616eaab648eeaf
muddabirm <33209109+muddabirm@users.noreply.github.com>
2024-09-22 17:10:38 -0500

    some navbar stuff

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+export default function Navbar() {
+    return (
+      <div className="flex flex-col  justify-start w-[30rem] min-h-screen p-8 gap-12 sm:p-20 text-3xl">
+        <ul className="space-y-4">
+            <li>
+              <Link href="#" className="transition ease-in duration-100 rounded-full py-3 px-6 hover:bg-gray-700">Home</Link>
+            </li>
+            <li>
+              <Link href="#" className="transition ease-in duration-100 rounded-full py-3 px-6 hover:bg-gray-700">Messages</Link>
+            </li>
+            <li>
+              <Link href="#" className="transition ease-in duration-100 rounded-full py-3 px-6 hover:bg-gray-700">Profile</Link>
+            </li>
+            <li>
+              <Link href="#" className="transition ease-in duration-100 rounded-full py-3 px-6 hover:bg-gray-700">Notificaitons</Link>
+            </li>
+            <li>
+              <Link href="#" className="transition ease-in duration-100 rounded-full py-3 px-6 hover:bg-gray-700">Settings</Link>
+            </li>
+        </ul>
+      </div>
+    );
+  }
