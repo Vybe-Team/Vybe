512fdf96ae690c6b18fb68e34af6d6362553b991
skrttao <srobertphong@gmail.com>
2024-09-25 18:36:19 -0500

    luicide icons

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image';
+import { Bell, User } from 'lucide-react';
 
 export default function Navbar() {
     return (
@@ -11,12 +12,12 @@ export default function Navbar() {
           <input type="search" id="search" className="w-full text-base rounded-2xl px-4 py-2 outline-none bg-[#f2f2f2]" placeholder="Search" />
         </form>
 
-        <ul className="flex w-1/3 h-full gap-x-2 justify-end items-center">
+        <ul className="flex w-1/3 h-full gap-x-2 justify-end items-center space-x-4">
           <li>
-            <Link href="#" className="transition ease-in duration-100 rounded-full py-3 px-6 hover:bg-gray-700">Bell Icon</Link>
+            <Link href="#" className="transition ease-in duration-100 rounded-full hover:bg-gray-700 hover:bg-gray-700 flex items-center p-2"><Bell/></Link>
           </li>
           <li>
-            <Link href="/login" className="transition ease-in duration-100 rounded-full py-3 px-6 hover:bg-gray-700">PF Icon</Link>
+            <Link href="/login" className="hover:bg-gray-700 flex items-center p-2 rounded-full"><User/></Link>
           </li>
         </ul>
       </nav>
